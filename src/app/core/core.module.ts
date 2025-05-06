1bc59d9d20533054d16c34a2d58748b7b01b55ac
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-06 18:35:51 +0100

    initialize core module with CommonModule import

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+// implement a core module
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [],
+  providers: []
+})
+export class CoreModule {}
