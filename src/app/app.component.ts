7af3310b2bf765e0784b4c778335603c4b5a93a1
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-11 15:13:43 +0100

    feat: add missing component imports to AppComponent for improved functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { WorkComponent } from "./components/work/work.component";
+import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { ContactMeComponent } from "./components/contact-me/contact-me.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from '@components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, AboutComponent, SkillsComponent, ExperienceComponent, WorkComponent, TestimonialsComponent, ContactMeComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
