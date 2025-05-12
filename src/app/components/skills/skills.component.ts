e9933d0056aee437d7edfa527f2f533f79853272
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-12 23:44:28 +0100

    feat: implement skills section with dynamic icon loading and dark mode support

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Skill } from '@models/interfaces';
+import { Subscription } from 'rxjs';
+import { ThemeService } from '@core/services/theme.service';
+import { getSkills } from '@core/utils/skills.utils';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit, OnDestroy {
+  private themeSubscription: Subscription | undefined;
+  skills: Skill[] = getSkills();
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeSubscription = this.themeService.isDarkTheme$.subscribe((isDark) => {
+      this.skills.forEach((skill) => {
+        skill.currentIcon = isDark  && skill.darkIcon ? skill.darkIcon : skill.lightIcon;
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription?.unsubscribe();
+  }
 }
