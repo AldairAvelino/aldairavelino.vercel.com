c69f02584193116310fe5b111bcb1c53414446dc
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-13 09:46:28 +0100

    feat: implement Work component with dynamic works list and responsive layout

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Work } from '@models/interfaces';
 
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work.component.html',
-  styleUrl: './work.component.scss'
+  styleUrl: './work.component.scss',
 })
 export class WorkComponent {
-
+  works: Work[] = [
+    {
+      title: 'Kibaku',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
+      image: 'assets/images/me/about.jpeg',
+      technologies: ['React', 'Next.js', 'TypeScript', 'Next.js', 'PostgreSQL', 'Tailwindcss', 'Figma', 'Cypress', 'Storybook', 'Git'],
+      liveUrl: 'https://example.com/kibaku',
+      githubUrl: 'https://github.com/username/kibaku'
+    },
+    {
+      title: 'Kibaku',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
+      image: 'assets/images/me/hero.jpeg',
+      technologies: ['React', 'Next.js', 'TypeScript', 'PostgreSQL', 'Tailwindcss', 'Figma', 'Cypress', 'Storybook', 'Git'],
+      liveUrl: 'https://example.com/kibaku',
+      githubUrl: 'https://github.com/username/kibaku'
+    },
+    {
+      title: 'Kibaku',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
+      image: 'assets/images/me/hero.jpeg',
+      technologies: ['React', 'Next.js', 'TypeScript', 'PostgreSQL', 'Tailwindcss', 'Figma', 'Cypress', 'Storybook', 'Git'],
+      liveUrl: 'https://example.com/kibaku',
+      githubUrl: 'https://github.com/username/kibaku'
+    }
+  ];
 }
