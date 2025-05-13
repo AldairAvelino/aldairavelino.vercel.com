77a0bd6b472b5b94dcf4a43251140f1cf5ed9d93
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-13 14:12:58 +0100

    feat: implement contact section with email and phone display, and copy functionality

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '@models/interfaces';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  contacts: Contact[] = [
+    {
+      email: 'aldair03avelino@gmail.com',
+      phone: '+244 948 901 733',
+    }
+  ];
 
+  copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text);
+  }
 }
