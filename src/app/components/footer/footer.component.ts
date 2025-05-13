b6c41be6dd7b1cf0eb5ba829ebbca9cdd97dc6f1
Aldair Avelino <aldair03avelino@gmail.com>
2025-05-13 14:28:52 +0100

    feat: implement footer component with current year and author display

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { getCurrentYear } from '@core/utils/year.utils';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  currentYear = getCurrentYear();
+  author: string = 'Aldair Avelino';
 }
