11506c1916aef9bed28864e540425b2af49e993b
Aldair Avelino <aldair03avelino@gmail.com>
2025-06-11 21:32:50 +0100

    feat: add utility function to retrieve social links

---
+++ src/app/core/utils/socials-link.utils.ts
@@ -0,0 +1,13 @@
+import { SocialLink } from '@models/interfaces';
+
+export function getSocialLinks(): SocialLink[] {
+  return [
+    {
+      github: 'https://github.com/AldairAvelino',
+      instagram: 'https://www.instagram.com/aldair_avelino',
+      twitter: 'https://x.com/aldair03avelino',
+      linkedin: 'https://www.linkedin.com/in/aldair-fernando-antonio-avelino',
+      youtube: 'https://www.youtube.com/@pequenodev'
+    }
+  ]
+}
