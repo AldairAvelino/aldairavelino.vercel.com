0172cf0e855f59e5f06522f3361b3080694247c7
Aldair Avelino <aldair03avelino@gmail.com>
2026-04-01 22:24:58 +0100

    feat(i18n): implement translation support for experience component
    - Replace hardcoded text with translation keys for badge and subtitle
    - Move experience data from component to language service for i18n support
    - Update component to use language service for dynamic content

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Experience } from '@models/interfaces';
+import { LanguageService } from '@core/services/language.service';
 
 @Component({
   selector: 'app-experience',
@@ -10,59 +10,9 @@ import { Experience } from '@models/interfaces';
   styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {
-  experiences: Experience[] = [
-    {
-      title: 'Sistema de Gestão de Monografias',
-      role: "FullStack Developer",
-      company: 'Aldair Avelino',
-      logo: 'assets/images/logo.svg',
-      startDate: 'Nov 2021',
-      endDate: 'Present',
-      responsibilities: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Ut pretium arcu et massa semper, id fringilla leo semper.',
-        'Sed quis justo ac magna.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      ]
-    },
-    {
-      title: 'Sistema de Gestão de Monografias',
-      role: "Backend Developer",
-      company: 'Aldair Avelino',
-      logo: 'assets/images/logo.svg',
-      startDate: 'Jul 2017',
-      endDate: 'Oct 2021',
-      responsibilities: [
-        'Sed quis justo ac magna.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Sed quis justo ac magna.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      ]
-    },
-    {
-      title: 'Sistema de Gestão de Monografias',
-      role: "FullStack Developer",
-      company: 'Aldair Avelino',
-      logo: 'assets/images/logo.svg',
-      startDate: 'Dec 2015',
-      endDate: 'May 2017',
-      responsibilities: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      ]
-    },
-    {
-      title: 'UX/UI Designer',
-      role: "Frontend Developer",
-      company: 'Aldair Avelino',
-      logo: 'assets/images/logo.svg',
-      startDate: 'Dec 2015',
-      endDate: 'May 2017',
-      responsibilities: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      ]
-    }
-  ];
+  constructor(public languageService: LanguageService) {}
+
+  get experiences() {
+    return this.languageService.translations.experience.list;
+  }
 }
