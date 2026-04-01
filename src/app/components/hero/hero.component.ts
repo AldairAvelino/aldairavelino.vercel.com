2f14dc84d93d29870318f9e3448f4456e7fa9859
Aldair Avelino <aldair03avelino@gmail.com>
2026-04-01 14:13:11 +0100

    use the randomize localStorage images

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,6 +2,7 @@ import { SocialLink } from '@models/interfaces';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getSocialLinks } from '@core/utils/socials-link.utils';
+import { getRandomPhoto } from '@core/utils/random-photo.util';
 
 @Component({
   selector: 'app-hero',
@@ -11,6 +12,19 @@ import { getSocialLinks } from '@core/utils/socials-link.utils';
   styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  randomPhotos: string[] = [
+    "assets/images/me/hero.jpeg",
+    "assets/images/me/blue-shirt.jpg"
+  ];
+
+  selectedPhoto!: string;
   socialLinks: SocialLink[] = getSocialLinks();
+
+  ngOnInit(): void {
+    this.selectedPhoto = getRandomPhoto(
+      this.randomPhotos,
+      'hero'
+    );
+  }
 
 }
